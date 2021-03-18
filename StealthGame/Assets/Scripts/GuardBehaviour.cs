a5142dbd93ee1dd7862fb0325309475ef4e01903
Gregor Milligan <gregormilligan123@gmail.com>
2021-03-18 19:12:47 +0000

    added the start to the bad guy behaviour

---
+++ StealthGame/Assets/Scripts/GuardBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardBehaviour : MonoBehaviour
+{
+    public Transform pathHolder;
+
+    private void Start()
+    {
+        Vector3[] waypoint  = new Vector3[pathHolder.childCount];
+        for (int i = 0; i < waypoint.Length; i++)
+        {
+            waypoint[i] = pathHolder.GetChild(i).position;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawSphere(waypoint.position, .2f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+}
